701a7a0f1dd792995138e8d3a31ca04ea6043536
six519 <ferdinandsilva@ferdinandsilva.com>
2020-06-22 19:45:48 +0800

    update build

--- Build/Android/src/com/ferdinandsilva/pusoydos3/OBBData.java
+++ Build/Android/src/com/ferdinandsilva/pusoydos3/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"2", // the version of the APK that the file was uploaded against
-869239075L // the length of the file in bytes
+"3", // the version of the APK that the file was uploaded against
+869470584L // the length of the file in bytes
 )
 };
 };
